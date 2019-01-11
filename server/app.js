eab867e3ab88a0268a3796a5ea1957971630081a
Zhanadil Nurtoleuov <znurtoleuov@gmail.com>
2019-01-11 22:28:38 +0000

    access control options added

--- server/app.js
+++ server/app.js
@@ -47,6 +47,13 @@ mongoose.connect(
 
 app.use(body_parser.json());
 app.use(fileUpload());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,  Access-Control-Allow-Headers, Authorization, X-Requested-With');
+    next();
+});
 app.use(cors());
 
 // Log all requests
